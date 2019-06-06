bug correction and double checking for lensing and foregrounds fisher computation, added ifs for foreground case in plotting et fisher computation (to be fixed to prevent conflicts), lensquest has been debbugged now lensing and birefringence seem indeed to commute, added in main and plot the foregrounds case as well as new plot comparing foregrounds/no foregrounds with different lmin

lib_project.py
@@ -47,6 +47,7 @@ def cl_rotation(cl, angle_in):
     cl : numpy array [number of l, number of spectra]
         Numpy array of the different power spectra in the order : TT, EE, BB, TE,
         and optionnally EB, TB
+        Takes both raw and l*(l+1)cl power spectra
     angle : float or numpy array
         angle of rotation IN RADIAN, can be isotropic is float or anisotropic if
         numpy array. In the latter case the angle is discomposed as fourier
@@ -291,6 +292,7 @@ def spectra_addition(spectra_dict, key1, key2):
 
 
 def get_fisher_dict( spectra_dict , angle_array, w_inv, beam_array, lensing = False, foregrounds = False, fisher_dict = {} ):
+    # TODO: same as get_truncated_fisher_dict
     if type(angle_array[0].shape) == ():
         angle_array = [angle_array]
     if fisher_dict == {}:
@@ -316,7 +318,32 @@ def get_fisher_dict( spectra_dict , angle_array, w_inv, beam_array, lensing = Fa
     return fisher_dict
 
 
-def get_truncated_fisher_dict(spectra_dict , angle_array, w_inv, beam_array, fisher_trunc_array_dict = {} ):
+def get_truncated_fisher_dict(spectra_dict , angle_array, w_inv, beam_array, foregrounds = False, fisher_trunc_array_dict = {} ):
+    """Short summary.
+
+    Parameters
+    ----------
+    spectra_dict : type
+        Description of parameter `spectra_dict`.
+    angle_array : type
+        Description of parameter `angle_array`.
+    w_inv : type
+        Description of parameter `w_inv`.
+    beam_array : type
+        Description of parameter `beam_array`.
+    foregrounds : type
+        Description of parameter `foregrounds`.
+    fisher_trunc_array_dict : type
+        Description of parameter `fisher_trunc_array_dict`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+        TODO : if fisher_trunc given diff than {} check for already created entries in dic so that not to create them twice
+               loop over keys in spectra dict to create fisher_trunc_array_dict (no need for extrea if statements)
+    """
+
     if type(angle_array[0].shape) == ():
         angle_array = [angle_array]
 
@@ -329,6 +356,12 @@ def get_truncated_fisher_dict(spectra_dict , angle_array, w_inv, beam_array, fis
         for k in beam_array :
             truncated_fisher_array, truncated_fisher_element_array = lib.truncated_fisher_angle(spectra_dict[0*u.deg], key_rot , cl_rot_noise = spectra_dict[(key_rot,(w_inv, k*u.arcmin))] , return_elements = True)
             fisher_trunc_array_dict[(key_rot,(w_inv, k*u.arcmin))] = truncated_fisher_element_array
+
+    if foregrounds == True:
+        for key_rot in angle_array:
+            for k in beam_array :
+                truncated_fisher_array, truncated_fisher_element_array = lib.truncated_fisher_angle(spectra_dict[0*u.deg], key_rot , cl_rot_noise = spectra_dict[((key_rot,(w_inv, k*u.arcmin)), 'foregrounds')] , return_elements = True)
+                fisher_trunc_array_dict[((key_rot,(w_inv, k*u.arcmin)), 'foregrounds')] = truncated_fisher_element_array
 
     return fisher_trunc_array_dict
 
@@ -368,8 +401,8 @@ def truncated_fisher_angle(cl_orig_for_deriv, angle, cl_rot_noise, f_sky = 1, re
             cov_matrix_inv = 1/(cl_rot_noise_truncated_array[i].T[2:])
 
         else:
+            cov_matrix_inv = np.linalg.inv( cl_rot_noise_truncated_array[i].T[2:])
 
-            cov_matrix_inv = np.linalg.inv( cl_rot_noise_truncated_array[i].T[2:] )
         cov_matrix_inv = cov_matrix_inv.T
 
         sq_in_trace=np.array([np.dot(cov_matrix_inv.T[k], cl_da_truncated_array[i].T[k+2])\
@@ -421,8 +454,8 @@ def get_foreground_spectrum(nside, nu_u, mask_file="HFI_Mask_GalPlane-apo2_2048_
     proper_sync_masked = hp.ma(synchrotron_signal)
     proper_sync_masked.mask = np.logical_not(mask)
 
-    cl_dust_masked = np.array([hp.anafast(proper_dust_masked, lmax = l_max + 1).T[l,:] *l*(l+1)/(2*np.pi) for l in range(l_max + 1)])
-    cl_synchrotron_masked = np.array([ hp.anafast(proper_sync_masked, lmax = l_max + 1).T[l,:] * l*(l+1)/(2*np.pi) for l in range(l_max + 1)])
+    cl_dust_masked = np.array([hp.anafast(proper_dust_masked, lmax = l_max).T[l,:] *l*(l+1)/(2*np.pi) for l in range(l_max )])
+    cl_synchrotron_masked = np.array([ hp.anafast(proper_sync_masked, lmax = l_max).T[l,:] * l*(l+1)/(2*np.pi) for l in range(l_max )])
 
     return {'dust':cl_dust_masked, 'synchrotron':cl_synchrotron_masked}
 

main_project.py
@@ -13,7 +13,7 @@ import pysm
 
 # l_max = 1734
 nside = 128
-l_max = nside*4 #1500
+l_max = nside*4 -1#1500
 raw_cl = False
 pars, results, powers = lib.get_basics(l_max, raw_cl)
 for name in powers: print(name)
@@ -40,14 +40,14 @@ for k in beam_array:
     for a in angle_array:
         lib.spectra_addition(spectra_dict, a, (w_inv, k * u.arcmin))
 
-lensing = False
-if lensing == True:
-    spectra_dict['lensed_scalar'] =lib.cl_rotation( powers['lensed_scalar'] ,0.*u.deg)
-    for k in beam_array:
-        for a in angle_array:
-            lib.spectra_addition(spectra_dict, (a, (w_inv, k * u.arcmin)) , 'lensed_scalar' )
+# lensing = False
+# if lensing == True:
+#     spectra_dict['lensed_scalar'] =lib.cl_rotation( powers['lensed_scalar'] ,0.*u.deg)
+#     for k in beam_array:
+#         for a in angle_array:
+#             lib.spectra_addition(spectra_dict, (a, (w_inv, k * u.arcmin)) , 'lensed_scalar' )
 
-foregrounds = True
+foregrounds = 0
 if foregrounds==True:
     foreground_dict = lib.get_foreground_spectrum(nside, 150*u.GHz)
     lib.spectra_addition(foreground_dict, 'dust','synchrotron')
@@ -57,6 +57,7 @@ if foregrounds==True:
         for a in angle_array:
             lib.spectra_addition(spectra_dict, (a, (w_inv, k * u.arcmin)), 'foregrounds')
 
+# print(np.linalg.inv(spectra_dict[((0.*u.deg, (w_inv,  0.* u.arcmin)), 'foregrounds')].T[2:] ) )
 # print('spectra dict keys=',spectra_dict.keys())
 angle_fisher_array = angle_array
 # print('angle_fisher_array=',angle_fisher_array)
@@ -64,7 +65,7 @@ beam_fisher_array = beam_array
 fisher_dict = lib.get_fisher_dict( spectra_dict ,angle_fisher_array , w_inv, beam_fisher_array, foregrounds= foregrounds)
 
 
-fisher_trunc_array_dict = lib.get_truncated_fisher_dict( spectra_dict, angle_fisher_array, w_inv, beam_fisher_array )
+fisher_trunc_array_dict = lib.get_truncated_fisher_dict( spectra_dict, angle_fisher_array, w_inv, beam_fisher_array , foregrounds = foregrounds)
 
 # ================================= test anisotropic angle======================
 # angle_anisotrop = np.random.normal(0,10, powers[spectrum].shape[0])
@@ -115,14 +116,30 @@ if plot_spectra == True:
 
 plot_truncated_fisher_cumlulative = 1
 if plot_truncated_fisher_cumlulative == True:
-    plotpro.truncated_fisher_cumulative(fisher_trunc_array_dict, (3*u.deg,(w_inv, 30.*u.arcmin)))
+    if foregrounds == True :
+        plotpro.truncated_fisher_cumulative(fisher_trunc_array_dict, ((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds'))
+    else:
+        plotpro.truncated_fisher_cumulative(fisher_trunc_array_dict, (3*u.deg, (w_inv, 30. * u.arcmin)))
+
     plt.show()
 
 plot_cumulative_3D = 1
 if plot_cumulative_3D == True:
-    surface, heatmap = plotpro.cumulative_error_3D(fisher_dict[((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds')],((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds'))
+    if foregrounds == True:
+        surface, heatmap = plotpro.cumulative_error_3D(fisher_dict[((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds')],((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds'))
+    else:
+        surface, heatmap = plotpro.cumulative_error_3D(fisher_dict[(3*u.deg, (w_inv, 30. * u.arcmin))],(3*u.deg, (w_inv, 30. * u.arcmin)))
+
     plt.show(surface)
     plt.show(heatmap)
+
+# foregrounds = 0
+if foregrounds == True:
+    fig_cumu = plotpro.cumulative_error(fisher_dict[(3*u.deg, (w_inv, 30. * u.arcmin))],label ='no foregrounds, lmin=5', l_min =5)
+    fig_cumu = plotpro.cumulative_error(fisher_dict[((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds')],label ='foregrounds, lmin=5', l_min =5)
+    fig_cumu = plotpro.cumulative_error(fisher_dict[(3*u.deg, (w_inv, 30. * u.arcmin))],label ='no foregrounds')
+    fig_cumu = plotpro.cumulative_error(fisher_dict[((3*u.deg, (w_inv, 30. * u.arcmin)),'foregrounds')],label ='foregrounds')
+    plt.show()
 
 # ((3*u.deg, (w_inv, 30. * u.arcmin)) , 'lensed_scalar')
 

plot_project.py
@@ -78,11 +78,14 @@ def error_on_angle_wrt_scale(fisher_element, label = None):
 
     return 0
 
-def cumulative_error(fisher_element, label = None):
-    ls = np.arange(2,len(fisher_element))
+def cumulative_error(fisher_element, label = None, l_min = 2):
+    ls = np.arange(l_min,len(fisher_element))
 
-    cumulative = np.array( [ 1 / np.sqrt( sum( fisher_element[:k]) ) for k in range(len(fisher_element)) ] )
-    plt.plot(ls, cumulative[2:], label=label)
+    cumulative = np.array( [ 1 / np.sqrt( sum( fisher_element[l_min:k]) ) for k in range(l_min,len(fisher_element)) ] )
+
+    plt.plot(ls, cumulative, label=label)
+
+
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel(r'$\ell$',fontsize = 16)
@@ -179,18 +182,24 @@ def truncated_fisher_cumulative(fisher_trunc_array_dict, key):
     for fisher_element in fisher_trunc_array_dict[key]:
         cumulative_error(fisher_element, label = label_list[count])
         count += 1
-    if key[1] == 'no noise':
+    if key[1] == 'foregrounds':
+        if key[0][1] == 'no noise':
+            plt.title( 'cumulative error for rotation {}, NO noise, and with foregrounds'.format(key[0][0]) )
+        else:
+            plt.title( 'cumulative error for rotation {}, noise beam {} and with foregrounds'.format(key[0][0], key[0][1][1]) )
+    elif key[1] == 'no noise':
         plt.title( 'cumulative error for rotation {}, and NO noise'.format(key[0]) )
     else :
         plt.title( 'cumulative error for rotation {}, and noise beam {}'.format(key[0], key[1][1]) )
     return 0
 
-def cumulative_error_3D(fisher_element, key):
+def cumulative_error_3D(fisher_element, key, l_max = None):
+    if l_max == None :
+        l_max = len(fisher_element)
+    l_min_array = np.arange(2,l_max)
+    l_max_array = np.arange(2,l_max)
 
-    l_min_array = np.arange(2,len(fisher_element))
-    l_max_array = np.arange(2,len(fisher_element))
-
-    slice_list = [ [ sum( fisher_element[l_min:l_max+1] )  for l_max in l_max_array ] for l_min in l_min_array]
+    slice_list = [ [ sum( fisher_element[_l_min:_l_max+1] )  for _l_max in l_max_array ] for _l_min in l_min_array]
     l_min_array , l_max_array = np.meshgrid(l_min_array , l_max_array, indexing = 'ij')
 
     cumulative = np.array(  1/np.sqrt(slice_list)  )
@@ -233,7 +242,7 @@ def cumulative_error_3D(fisher_element, key):
         else:
             ax.set_title( 'surface plot of cumulative error for rotation {}, and noise beam {}, with lensing'.format(key[0][0], key[0][1][1]) )
             ax2.set_title( 'heatmap of cumulative error for rotation {}, and noise beam {}, with lensing'.format(key[0][0], key[0][1][1]) )
-    if key[1]=='foregrounds' :
+    elif key[1]=='foregrounds' :
         if key[0][1] == 'no noise':
             ax.set_title( 'surface plot of cumulative error for rotation {}, and NO noise, with foregrounds'.format(key[0][0]) )
             ax2.set_title( 'heatmap of cumulative error for rotation {}, and NO noise, with foregrounds'.format(key[0][0]) )

test_lensquest.py
@@ -14,8 +14,10 @@ import lensquest as lq
 
 l_max = 1500
 raw_cl = False
-pars, results, powers, lens_potential_spectrum = lib.get_basics(l_max, raw_cl, lens_potential = True)
+pars, results, powers = lib.get_basics(l_max, raw_cl)#, lens_potential = True)
 for name in powers: print(name)
+
+lens_potential_spectrum = powers['lens_potential']
 
 lensed_spectrum_test =lib.cl_rotation( lib.cl_normalisation(powers['total']), 1*u.deg)
 print('lensed_spectrum_test shape',np.shape(lensed_spectrum_test))